73515a9ef4e4cbee32b5fef7215414371f7ea4f8
karankraina <karanraina1996@gmail.com>
2018-12-08 11:10:15 +0530

    𑆠𑆟𑆛𑆫𑆟𑆓

---
+++ Controllers/application.js
@@ -0,0 +1,75 @@
+const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
+const contentful = require('contentful');
+var exphbs = require('express-handlebars');
+var url = require('url');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+
+
+
+const vaakh = function(){
+    title = '';
+    hindiImage = '';
+    shardaImage = '';
+    audio = '';
+    englishTransliteration = '';
+    wordMeaning = '';
+    summary = '';
+    additionalCommentary = '';
+}
+
+
+
+// Contentfill Client 
+const client = contentful.createClient({
+    space: 'hmdpqvmg445m',
+    //environment: '<environment_id>', // defaults to 'master' if not set
+    accessToken: 'e4cb8c0f3b9d5370ace3361bedfe93a3d72d714f681811fb3973b03b8c86baba'
+})
+
+function runapplication(req, res){
+    const vaakharray = [];
+    client.getEntry('2OwwF5tykgG0IASeo2auAi')
+     .then((entry) => {
+        //  console.log(entry.fields);
+        entry.fields.vaakh.forEach((item) => {
+            let vaakhtemp = new vaakh();
+            if (item.fields.title !== undefined){
+                vaakhtemp.title = item.fields.title
+            }
+            if (item.fields.hindiImage !== undefined){
+                vaakhtemp.hindiImage = item.fields.hindiImage.fields.file.url
+            }
+            if (item.fields.shardaImage !== undefined){
+                vaakhtemp.shardaImage = item.fields.shardaImage.fields.file.url
+            }
+            if (item.fields.audio !== undefined){
+                vaakhtemp.audio = item.fields.audio.fields.file.url
+            }
+            if (item.fields.englishTransliteration !== undefined){
+                vaakhtemp.englishTransliteration = item.fields.englishTransliteration
+            }
+            if (item.fields.wordMeaning !== undefined){
+                vaakhtemp.wordMeaning = item.fields.wordMeaning
+            }
+            if (item.fields.summary !== undefined){
+                vaakhtemp.summary = item.fields.summary
+            }
+            if (item.fields.additionalCommentary !== undefined){
+                vaakhtemp.additionalCommentary = item.fields.additionalCommentary
+            }
+            vaakharray.push(vaakhtemp);
+        })
+        // global.vaakharrayglobal = vaakharray;
+        console.log(vaakharray)
+        res.render('home', {data: vaakharray[req.query.number], vaakharray});
+
+
+     }).catch(console.error)
+}
+module.exports = {
+    runapplication
+}
--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-
 const PORT = process.env.PORT || 5000
 var express = require('express');
 var app = express();
+const application = require('./Controllers/application')
 var exphbs = require('express-handlebars');
-
+var url = require('url');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
+
+
+     
 app.get('/', (req, res) => {
+    var k = url.parse(req.url)
+    //console.log(k)
+    
+    //console.log(typeof k.name);
     res.render('home');
+});
+// Vaakh Code
+app.get('/vaakh', (req, res) => {
+    // console.log(req.query.number);
+    application.runapplication(req, res)
 })
 
 app.listen(PORT, () => {
-    console.log("Server Created at 8080")
+    console.log("Server Created at 5000")
 })
+
